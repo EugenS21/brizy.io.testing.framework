cc0416e41c9ebc4496ddee363c07389779763fd6
Eugeniu Savca <savcaeugen@gmail.com>
2023-06-16 10:44:41 +0300

    * Added new dependencies to reporting module; * Added configurations for allure reporting; * Added the possibility to log information about the environment on startup; * Removed unused yaml file.

--- web-test/src/test/java/org/eugens21/brizy/io/web/test/config/BrizyTestsConfig.java
+++ web-test/src/test/java/org/eugens21/brizy/io/web/test/config/BrizyTestsConfig.java
@@ -1,11 +1,31 @@
 package org.eugens21.brizy.io.web.test.config;
 
 import io.cucumber.spring.CucumberContextConfiguration;
+import jakarta.annotation.PostConstruct;
 import org.eugens21.brizy.io.web.WebDriverConfiguration;
 import org.eugens21.brizy.io.web.common.WebCommonConfiguration;
 import org.eugens21.brizy.io.web.interactions.WebInteractionsConfiguration;
+import org.eugens21.brizy.io.web.reporting.WebReportingConfiguration;
+import org.eugens21.brizy.io.web.reporting.service.EnvironmentPropertiesWriter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.nio.file.Path;
+import java.util.Map;
+
 @CucumberContextConfiguration
-@SpringBootTest(classes = {WebInteractionsConfiguration.class, WebDriverConfiguration.class, WebCommonConfiguration.class})
-public class BrizyTestsConfig {}
+@SpringBootTest(classes = {WebInteractionsConfiguration.class, WebDriverConfiguration.class, WebCommonConfiguration.class, WebReportingConfiguration.class})
+public class BrizyTestsConfig {
+
+    @Autowired
+    EnvironmentPropertiesWriter environmentPropertiesWriter;
+
+    @PostConstruct
+    public void initProperties() {
+        environmentPropertiesWriter.writeProperties(Path.of("target/allure-results/environment.properties"), Map.of(
+                "Executor", System.getProperty("os.name"),
+                "Java", System.getProperty("java.version")
+        ));
+    }
+
+}
